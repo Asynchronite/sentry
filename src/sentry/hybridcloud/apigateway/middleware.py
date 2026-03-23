7f209926174b074370849d2da7c549fd3af16ba2
Giovanni Barillari <giovanni.barillari@sentry.io>
2026-03-23 12:36:59 +0100

    wip

--- src/sentry/hybridcloud/apigateway/middleware.py
+++ src/sentry/hybridcloud/apigateway/middleware.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from collections.abc import Callable
 from typing import Any
 
+# from asgiref.sync import iscoroutinefunction, markcoroutinefunction
 from django.http.response import HttpResponseBase
 from rest_framework.request import Request
 
@@ -12,20 +13,25 @@ from sentry.hybridcloud.apigateway import proxy_request_if_needed
 class ApiGatewayMiddleware:
     """Proxy requests intended for remote silos"""
 
+    async_capable = True
+    sync_capable = False
+
     def __init__(self, get_response: Callable[[Request], HttpResponseBase]):
         self.get_response = get_response
+        # if iscoroutinefunction(self.get_response):
+        #    markcoroutinefunction(self)
 
     def __call__(self, request: Request) -> HttpResponseBase:
         return self.get_response(request)
 
-    def process_view(
+    async def process_view(
         self,
         request: Request,
         view_func: Callable[..., HttpResponseBase],
         view_args: tuple[str],
         view_kwargs: dict[str, Any],
     ) -> HttpResponseBase | None:
-        proxy_response = proxy_request_if_needed(request, view_func, view_kwargs)
+        proxy_response = await proxy_request_if_needed(request, view_func, view_kwargs)
         if proxy_response is not None:
             return proxy_response
         else:
